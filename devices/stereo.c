36304697d62419ce91fc8878a9b014e9797c097d
Jan Samek <vosudo@gmail.com>
2026-03-08 00:48:23 +0100

    Finished commenting code

--- devices/stereo.c
+++ devices/stereo.c
@@ -48,73 +48,80 @@ static void get_samples(void) {
 bool load_stereo(Device *self) {
     ringbuffer_init(STEREO_RINGBUFFER_SIZE);
 
+    // Load PIO programs
     sound_left_offset = pio_manager_load(&sound_left_pio, &sound_left_sm, &stereo_left_program);
+
     if (sound_left_offset < 0) {
         return false;
     }
 
     sound_right_offset = pio_manager_load(&sound_right_pio, &sound_right_sm, &stereo_right_program);
+
     if (sound_right_offset < 0) {
         return false;
     }
 
     detection_offset = pio_manager_load(&detection_pio, &detection_sm, &stereo_detection_program);
+
     if (detection_offset < 0) {
         return false;
     }
 
+    // Configure left channel PIO GPIO pins
     pio_sm_config used_config_left = stereo_left_program_get_default_config(sound_left_offset);
     sm_config_set_in_pins(&used_config_left, LPT_BASE_PIN);
     sm_config_set_fifo_join(&used_config_left, PIO_FIFO_JOIN_RX);
 
-    for (int i = LPT_BASE_PIN; i < LPT_BASE_PIN + 8; i++) { // Sets pins to use PIO
+    for (int i = LPT_BASE_PIN; i < LPT_BASE_PIN + 8; i++) { // Set all data pins
         pio_gpio_init(sound_left_pio, i);
     }
 
     pio_gpio_init(sound_left_pio, LPT_AUTOFEED_PIN);
+    pio_sm_set_consecutive_pindirs(sound_left_pio, sound_left_sm, LPT_BASE_PIN, 8, false); // Set pins in PIO to be inputs
 
-    pio_sm_set_consecutive_pindirs(sound_left_pio, sound_left_sm, LPT_BASE_PIN, 8, false); // Sets pins in PIO to be inputs
-
+    // Start left channel PIO program
     if (pio_sm_init(sound_left_pio, sound_left_sm, sound_left_offset, &used_config_left) < 0) {
         return false;
     }
 
     pio_sm_set_enabled(sound_left_pio, sound_left_sm, true);
 
+    // Configure right channel PIO GPIO pins
     pio_sm_config used_config_right = stereo_right_program_get_default_config(sound_right_offset);
     sm_config_set_in_pins(&used_config_right, LPT_BASE_PIN);
     sm_config_set_fifo_join(&used_config_right, PIO_FIFO_JOIN_RX);
 
-    for (int i = LPT_BASE_PIN; i < LPT_BASE_PIN + 8; i++) { // Sets pins to use PIO
+    for (int i = LPT_BASE_PIN; i < LPT_BASE_PIN + 8; i++) { // Set all data pins
         pio_gpio_init(sound_right_pio, i);
     }
 
     pio_gpio_init(sound_right_pio, LPT_STROBE_PIN);
+    pio_sm_set_consecutive_pindirs(sound_right_pio, sound_right_sm, LPT_BASE_PIN, 8, false); // Set pins in PIO to be inputs
 
-    pio_sm_set_consecutive_pindirs(sound_right_pio, sound_right_sm, LPT_BASE_PIN, 8, false); // Sets pins in PIO to be inputs
-
+    // Start right channel PIO program
     if (pio_sm_init(sound_right_pio, sound_right_sm, sound_right_offset, &used_config_right) < 0) {
         return false;
     }
 
     pio_sm_set_enabled(sound_right_pio, sound_right_sm, true);
 
+    // Configure detection PIO GPIO pins
     pio_sm_config used_config_detection = stereo_detection_program_get_default_config(detection_offset);
-    sm_config_set_in_pins(&used_config_detection, LPT_BASE_PIN + 7);
+    sm_config_set_in_pins(&used_config_detection, LPT_BASE_PIN + 7); // Requires D7 pin of LPT
     sm_config_set_out_pins(&used_config_detection, LPT_BUSY_PIN, 1);
-
     pio_gpio_init(detection_pio, LPT_BASE_PIN + 7);
     pio_gpio_init(detection_pio, LPT_BUSY_PIN);
-
     pio_sm_set_consecutive_pindirs(detection_pio, detection_sm, LPT_BASE_PIN + 7, 1, false);
     pio_sm_set_consecutive_pindirs(detection_pio, detection_sm, LPT_BUSY_PIN, 1, true);
 
+    // Start detection PIO program
     if (pio_sm_init(detection_pio, detection_sm, detection_offset, &used_config_detection) < 0) {
         return false;
     }
 
     pio_sm_set_enabled(detection_pio, detection_sm, true);
 
+    // Run precise generation of sample take
     gpio_set_function(PICO_UNUSED_PIN, GPIO_FUNC_PWM);
     pwm_slice = pwm_gpio_to_slice_num(PICO_UNUSED_PIN);
     uint16_t top = (clock_get_hz(clk_sys) + SAMPLE_RATE / 2) / SAMPLE_RATE - 1;
@@ -126,15 +133,17 @@ bool load_stereo(Device *self) {
     irq_set_exclusive_handler(PWM_IRQ_WRAP, get_samples);
     irq_set_enabled(PWM_IRQ_WRAP, true);
     pwm_set_enabled(pwm_slice, true);
-
     return true;
 }
 
 bool unload_stereo(Device *self) {
+
+    // Stop getting samples
     pwm_set_enabled(pwm_slice, false);
     irq_set_enabled(PWM_IRQ_WRAP, false);
     irq_remove_handler(PWM_IRQ_WRAP, get_samples);
 
+    // Stop PIO programs
     pio_sm_set_enabled(sound_left_pio, sound_left_sm, false);
     pio_sm_set_enabled(sound_right_pio, sound_right_sm, false);
     pio_sm_set_enabled(detection_pio, detection_sm, false);
@@ -142,10 +151,11 @@ bool unload_stereo(Device *self) {
     pio_manager_unload(sound_right_pio, sound_right_sm, sound_right_offset, &stereo_right_program);
     pio_manager_unload(detection_pio, detection_sm, detection_offset, &stereo_detection_program);
 
-
+    // Deinit all GPIO pins
     for (int i = LPT_BASE_PIN; i < LPT_BASE_PIN + 8; i++) {
         gpio_deinit(i);
     }
+
     gpio_deinit(LPT_STROBE_PIN);
     gpio_deinit(LPT_AUTOFEED_PIN);
     gpio_deinit(LPT_BUSY_PIN);
@@ -154,11 +164,16 @@ bool unload_stereo(Device *self) {
 
 size_t generate_stereo(Device *self, int16_t *left_sample, int16_t *right_sample) { 
 
+    // Wait for sample if none is generated (cannot lock since samples are generated automatically)
     while (!ringbuffer_pop(left_sample)) {
         tight_loop_contents();
     }
 
-    ringbuffer_pop(right_sample);
+    // Cannot happen - added as a failsafe
+    if (!ringbuffer_pop(right_sample)) {
+        *right_sample = 0;
+    }
+
     return 0;
 }
 
